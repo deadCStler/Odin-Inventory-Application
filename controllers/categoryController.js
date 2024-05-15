7df87e6b0fc976bdd7160f12ba9d45fdb16640ce
deadCStler <chitranshsrivastava01@gmail.com>
2024-05-15 17:51:02 +0530

    added orms and build out the controllers for CRUD && project finished

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const Category = require("../models/category");
 const Weapon = require("../models/weapons");
+const { body, validationResult } = require("express-validator");
+const { render } = require("../app");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const [numCategory, numWeapons] = await Promise.all([
@@ -41,26 +43,126 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Test for Category Create get");
-});
+exports.category_create_get = (req, res, next) => {
+  res.render("category_form", { title: "Create Category" });
+};
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Test for Category Create post");
-});
+exports.category_create_post = [
+  body("name", "Categroy must conatain atleast three letter")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Create Categroy",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const categoryExists = await Category.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+
+      if (categoryExists) {
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-  res.send("Test for Category Update get");
+  const category = await Category.findById(req.params.id).exec();
+
+  if (category === null) {
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("category_form", { title: "Update Category", category: category });
 });
 
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-  res.send("Test for Category Update post");
-});
+exports.category_update_post = [
+  body("name", "Categroy must conatain atleast three letter")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Create Categroy",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await Category.findByIdAndUpdate(req.params.id, category);
+      res.redirect(category.url);
+    }
+  }),
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("Test for Category Delete get");
+  const [category, allWeaponsByCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Weapon.find({ category: req.params.id }, "name description").exec(),
+  ]);
+
+  if (category === null) {
+    res.redirect("/menu/categories");
+  }
+
+  res.render("category_delete", {
+    title: "Delete Category",
+    category: category,
+    category_weapons: allWeaponsByCategory,
+  });
 });
 
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("Test for Category Delete post");
+  const [category, allWeaponsByCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Weapon.find({ category: req.params.id }, "name description").exec(),
+  ]);
+
+  if (allWeaponsByCategory.length > 0) {
+    res.render("category_delete", {
+      title: "Delete Category",
+      category: category,
+      category_weapons: allWeaponsByCategory,
+    });
+    return;
+  } else {
+    await Category.findByIdAndDelete(req.body.categoryid);
+    res.redirect("/menu/categories");
+  }
 });
--- controllers/weaponController.js
+++ controllers/weaponController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Weapon = require("../models/weapons");
+const Category = require("../models/category");
+const { body, validationResult } = require("express-validator");
 
 exports.weapon_list = asyncHandler(async (req, res, next) => {
   const allWeapons = await Weapon.find({}, "name").exec();
@@ -25,25 +27,135 @@ exports.weapon_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.weapon_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Test for Weapon Create get");
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+  res.render("weapon_form", {
+    title: "Create Weapon",
+    categories: allCategories,
+  });
 });
 
-exports.weapon_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Test for Weapon Create post");
-});
+exports.weapon_create_post = [
+  body("name", "Weapon name must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Weapon description must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Invalid price").isNumeric(),
+  body("category.*").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const weapon = new Weapon({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().exec();
+
+      res.render("weapon_form", {
+        title: "Create Weapon",
+        categories: allCategories,
+        weapon: weapon,
+        errors: errors.array(),
+      });
+    } else {
+      await weapon.save();
+      res.redirect(weapon.url);
+    }
+  }),
+];
 
 exports.weapon_update_get = asyncHandler(async (req, res, next) => {
-  res.send("Test for Weapon Update get");
+  const [weapon, allCategories] = await Promise.all([
+    Weapon.findById(req.params.id).populate("category").exec(),
+    Category.find().exec(),
+  ]);
+
+  if (weapon === null) {
+    const error = new Error("Weapon not found");
+    error.status = 404;
+    return next(error);
+  }
+  res.render("weapon_form", {
+    title: "Update Weapon",
+    categories: allCategories,
+    weapon: weapon,
+  });
 });
 
-exports.weapon_update_post = asyncHandler(async (req, res, next) => {
-  res.send("Test for Weapon Update post");
-});
+exports.weapon_update_post = [
+  body("name", "Weapon name must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Weapon description must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Invalid price").isNumeric(),
+  body("category.*").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const weapon = new Weapon({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().exec();
+
+      res.render("weapon_form", {
+        title: "Create Weapon",
+        categories: allCategories,
+        weapon: weapon,
+        errors: errors.array(),
+      });
+    } else {
+      const updatedWeapon = await Weapon.findByIdAndUpdate(
+        req.params.id,
+        weapon
+      );
+      res.redirect(updatedWeapon.url);
+    }
+  }),
+];
 
 exports.weapon_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("Test for Weapon Delete get");
+  const weapon = await Weapon.findById(req.params.id)
+    .populate("category")
+    .exec();
+
+  if (weapon === null) {
+    res.redirect("/menu/weapons");
+  }
+  res.render("weapon_delete", {
+    title: "Delete Weapon",
+    weapon: weapon,
+  });
 });
 
 exports.weapon_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("Test for Weapon Delete post");
+  const weapon = await Weapon.findById(req.params.id)
+    .populate("category")
+    .exec();
+
+  if (weapon === null) {
+    res.redirect("/menu/weapons");
+  }
+
+  await Weapon.findByIdAndDelete(req.body.weaponid);
+  res.redirect("/menu/weapons");
 });
